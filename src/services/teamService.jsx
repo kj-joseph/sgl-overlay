a266c1acfc2a348ecfa369be4f312d590a942f25
Kevin Joseph <kj.joseph@gmail.com>
2025-02-25 21:51:00 -0800

    Concatenate two logo fields for long base64 urls that don't fit in a single Google Sheets cell

--- src/services/teamService.jsx
+++ src/services/teamService.jsx
@@ -6,7 +6,16 @@ export const getTeamList = async () =>
 
 		callSheets("teams", {})
 			.then((response) =>
-				resolve(response))
+				resolve(response.map(team => ({
+					name: team.name,
+					soccerTeamAbbreviation: team.soccerTeamAbbreviation,
+					soccerTeamCity: team.soccerTeamCity,
+					soccerTeamName: team.soccerTeamName,
+					bgColor: team.bgColor,
+					league: team.league,
+					logo: `${team.logo1}${team.logo2}`,
+				})))
+			)
 
 			.catch((error) =>
 				reject(error));
