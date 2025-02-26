4f44af44b5b8ea715fd46af51f6d13595af8801d
Kevin Joseph <kj.joseph@gmail.com>
2025-02-25 21:17:16 -0800

    Fix issue with base64 image URLs

--- src/utils/imageLocation.js
+++ src/utils/imageLocation.js
@@ -1,7 +1,7 @@
 const imageLocation = (data, directory) =>
 	data.substr(0,8) === "https://"
 	|| data.substr(0,7) === "http://"
-	|| data.substr(0,10) === "data:image/" ?
+	|| data.substr(0,11) === "data:image/" ?
 		data
 	: `/${directory}/${data}`
 
