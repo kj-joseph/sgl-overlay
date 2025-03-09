87612b2d5e1aef4447d5e88d0e72fcb1fd72981e
Kevin Joseph <kj.joseph@gmail.com>
2025-03-09 15:29:37 -0700

    Use module for sgl style-wide variables

--- src/views/overlay/Overlay.jsx
+++ src/views/overlay/Overlay.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useParams } from "react-router-dom";
 import useWebSocket from "react-use-websocket";
 
 import defaultConfig from "@/data/config.json";
